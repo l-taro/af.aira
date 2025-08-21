3e8cc391de9feb6cf8575f8b1530a2a51c848053
lyudmila <lyudmilataro@gmail.com>
2025-08-21 21:06:05 +0500

    final stage

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
-$(document).ready(function() {  
-
-    $(document).on("click", ".tab-item", function() {
-        $('body').removeAttr('class');
-        var cl = $(this).attr('data-class');
-        $('body').addClass(cl);
-        $('.tab-item').toggleClass('active');
-    }); 
+$(document).ready(function() { 
 
     $(document).on("click", ".cb__cart_button", function() {        
         $('.cb__content').toggleClass('cart');
         $(this).toggleClass('active');
     }); 
 
-    $(document).on("click", "#search_close", function() {        
-        //$('#search_results').toggleClass('open');
-        $('#search_results').slideToggle('fast');
+    $(document).on("click", ".search_close", function() { 
+        $(this).next('.search_results').slideToggle('fast');
         $(this).toggleClass('open');
     }); 
-
     
 });
